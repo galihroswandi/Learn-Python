9969f093bd32ca4d9e0015080b3f3f208f5c6dcb
galihroswandi <galihroswandi25@gmail.com>
2024-01-25 17:55:13 +0700

    feat: Data dictionary (#38)

---
+++ 38.DataDictionary/Main.py
@@ -0,0 +1,24 @@
+
+
+# List
+data_list = ["ucup", "otong", "dudung"]
+
+print(data_list[0])
+
+# dictionary (dict) -> associative array / object
+# identifier / key : value
+data_dict = {
+    'key': 'value',
+    "cp": "Ucup",
+    "ot": "Otong",
+    "Dd": "Dudung",
+    "nmbr": 100,
+    "list": data_list
+}
+
+print(data_dict["key"])
+print(data_dict["cp"])
+print(data_dict["ot"])
+print(data_dict["Dd"])
+print(data_dict["nmbr"])
+print(data_dict["list"])
