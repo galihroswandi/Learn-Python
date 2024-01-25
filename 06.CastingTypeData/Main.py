6a9958ad2c054a8c61e9400d6eaf27146bb02922
galihroswandi <galihroswandi25@gmail.com>
2024-01-25 11:17:36 +0700

    feat: Casting / merubah tipe data (#06)

---
+++ 06.CastingTypeData/Main.py
@@ -0,0 +1,13 @@
+# Casting --- merubah dari dari satu tipe data ke tipe data yang lain
+
+# int
+valueA = 200
+
+valueFloat = float(valueA)
+valueString = str(valueA)
+valueBool = bool(valueA)
+
+print("Data = ", valueA, "Bertipe = ", type(valueA))
+print("Data = ", valueFloat, "Bertipe = ", type(valueFloat))
+print("Data = ", valueString, "Bertipe = ", type(valueString))
+print("Data = ", valueBool, "Bertipe = ", type(valueBool))
