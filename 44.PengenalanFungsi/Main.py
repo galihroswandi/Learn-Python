59d0ae5a024c959852a8ee5df2a8c0990da6e88e
galihroswandi <galihroswandi25@gmail.com>
2024-01-28 09:16:49 +0700

    feat: Pengenalan Fungsi (#44)

---
+++ 44.PengenalanFungsi/Main.py
@@ -0,0 +1,17 @@
+'''Pengenalan Fungsi'''
+
+def hello_world():
+    print("Hello world")
+    print("From Otong surotong")
+    print("To Ucup surucup")
+
+hello_world()
+hello_world()
+hello_world()
+
+
+def fungsi():
+    '''Pemanggilan fungsi ini harus setelah dibuat'''
+    print("Fungsi dipanggil")
+
+fungsi()
